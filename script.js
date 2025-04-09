e807b5c0ba69185bccf54fc7e25f5d603a50ed19
Moritz Wendt <moritz_wendt@icloud.com>
2025-04-09 21:32:12 +0200

    v0.2.5.3

--- script.js
+++ script.js
@@ -33,9 +33,6 @@ losGehts.addEventListener('click', () => {
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-var disp = document.getElementsByClassName("disp");
-var top = document.getElementsByClassName("top");
-
 
 btn.onclick = function() {
     modal.style.display = "block";
